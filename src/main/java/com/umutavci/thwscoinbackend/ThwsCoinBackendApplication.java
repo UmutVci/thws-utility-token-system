45eefaca24e01f38e9837bdaa959a471d81124e2
umutVci <umutavci03@gmail.com>
2025-12-02 16:59:59 +0100

    spring boot initialized

---
+++ src/main/java/com/umutavci/thwscoinbackend/ThwsCoinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umutavci.thwscoinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThwsCoinBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThwsCoinBackendApplication.class, args);
+    }
+
+}
